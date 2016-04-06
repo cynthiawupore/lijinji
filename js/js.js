4151e27a6b259601f14d4f8c1592859bfe942a76
cynthiawupore <cynthiawuqian@outlook.com>
2016-04-06 17:29:57 +0800

    李锦记

---
+++ js/js.js
@@ -0,0 +1,153 @@
+// main-left
+function map(){
+	$(".map").fadeIn(1000);
+}
+function point(){
+	$(".point").fadeIn(500);
+	$(".point").addClass('flash'); 
+}
+function line(){
+	$(".line").fadeIn(500);
+}
+function china(){
+	$(".china").fadeIn(500);
+}
+function flag(){
+	$(".flag").fadeIn(500);
+	$(".flag").addClass('bounceInDown');
+}
+function description(){
+	$(".description").fadeIn(500);
+	$(".description").addClass('rollIn');
+	
+}
+setTimeout(map,0)
+setTimeout(point,1000)
+setTimeout(line,1500)
+setTimeout(china,2000)
+setTimeout(flag,2500)
+setTimeout(description,3500)
+
+
+// right top section1
+function mrt1_title(){
+	$(".mrt1-title").fadeIn(500);
+}
+function mrt1_history(){
+	$(".mrt1-history").fadeIn(500);
+	$(".mrt1-history").addClass('pulse');
+}
+function mrt1_pointTop(){
+	$(".mrt1-pointTop").fadeIn(500);
+}
+function mrt1_pointBtm(){
+	$(".mrt1-pointBtm").fadeIn(500);
+}
+function mrt1_words(){
+	$(".mrt1-words").fadeIn(500);
+}
+setTimeout(mrt1_title,4000)
+setTimeout(mrt1_history,4500)
+setTimeout(mrt1_pointTop,5000)
+setTimeout(mrt1_pointBtm,5500)
+setTimeout(mrt1_words,6000)
+
+
+// right top section2
+function mrt2_h1(){
+	$(".main-right-top2 h1").fadeIn(100);
+}
+function mrt2_product(){
+	$(".mrt2-product").fadeIn(500);
+	$(".mrt2-product").addClass('mrt2-product'); //2s
+}
+function mrt2_words(){
+	$(".mrt2-words").fadeIn(500);
+}
+setTimeout(mrt2_h1,6500)
+setTimeout(mrt2_product,6600)
+setTimeout(mrt2_words,7100)
+
+
+// right top section3
+function mrt3_title(){
+	$(".mrt3-title").fadeIn(100);
+}
+function mrt3_cyclic(){
+	$(".mrt3-cyclic").fadeIn(500);
+}
+function mrt3_column(){
+	$(".mrt3-column").fadeIn(500);
+	$(".mrt3-column").addClass('zoomIn');
+}
+setTimeout(mrt3_title,7600)
+setTimeout(mrt3_cyclic,7700)
+setTimeout(mrt3_column,8200)
+
+
+// right top section4
+function mrt4_title(){
+	$(".mrt4-title").fadeIn(100);
+}
+function mrt4_bg(){
+	$(".mrt4-bg").fadeIn(500);
+	$(".mrt4-bg").addClass('mrt4_bg'); //3s
+
+}
+setTimeout(mrt4_title,8700)
+setTimeout(mrt4_bg,9000)
+
+
+
+// right bottom section1
+function mrb_title(){
+	$(".mrb-title").fadeIn(100);
+	$(".mrb-title").addClass('bounceInLeft');
+}
+function main_right_btm1(){
+	$(".main-right-btm1-box").fadeIn(500);
+	$(".main-right-btm1-box").addClass('main-right-btm1'); //3s
+}
+setTimeout(mrb_title,8700)
+setTimeout(main_right_btm1,8800)
+
+
+// right bottom section2
+function mrb2_grids(){
+	$(".mrb2-grids").fadeIn(500);
+}
+function mrb2_lineBlue(){
+	$(".mrb2-lineBlue").fadeIn(500);
+}
+function mrb2_lineYellow(){
+	$(".mrb2-lineYellow").fadeIn(500);
+}
+setTimeout(mrb2_grids,11800)
+setTimeout(mrb2_lineBlue,12300)
+setTimeout(mrb2_lineYellow,12800)
+
+// right bottom section3
+function mrb3_map(){
+	$(".mrb3-map").fadeIn(500);
+	$(".mrb3-map").addClass('bounceIn'); 
+}
+function mrb3_point(){
+	$(".mrb3-point").fadeIn(500);
+}
+setTimeout(mrb3_map,13300)
+setTimeout(mrb3_point,13800)
+
+// right bottom section4
+function mrb4_coords(){
+	$(".mrb4-coords").fadeIn(500);
+}
+function mrb4_map(){
+	$(".mrb4-map").fadeIn(500);
+	$(".mrb4-map").addClass('flash'); 
+}
+function mrb4_line(){
+	$(".mrb4-line").fadeIn(500);
+}
+setTimeout(mrb4_coords,14300)
+setTimeout(mrb4_map,14700)
+setTimeout(mrb4_line,15200)
